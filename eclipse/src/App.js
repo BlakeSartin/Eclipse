fbbd0d9c04a25b2ada5d2a2200cf4f6bbebce88b
Blake Sartin <blake.sartin8@gmail.com>
2022-12-30 20:52:59 -0800

    Update App.js

--- eclipse/src/App.js
+++ eclipse/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Topbar from "./components/Topbar";
 
 function App() {
   return (
     <div className="App">
+      <Topbar />
     </div>
   );
 }
 
 export default App;
+
