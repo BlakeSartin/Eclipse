f25714c2a1e5990329564f5f2a45c2fa3499ff62
Blake Sartin <blake.sartin8@gmail.com>
2023-01-05 17:24:17 -0800

    add use state function to switch between styling on topbar

--- eclipse/src/components/Topbar.js
+++ eclipse/src/components/Topbar.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Topbar.scss";
-import WbSunnyTwoToneIcon from '@mui/icons-material/WbSunnyTwoTone';
-import AccountCircleTwoToneIcon from '@mui/icons-material/AccountCircleTwoTone';
-import ChatTwoToneIcon from '@mui/icons-material/ChatTwoTone';
+import WbSunnyTwoToneIcon from "@mui/icons-material/WbSunnyTwoTone";
+import AccountCircleTwoToneIcon from "@mui/icons-material/AccountCircleTwoTone";
+import ChatTwoToneIcon from "@mui/icons-material/ChatTwoTone";
 
 export default function Topbar() {
+  const [isActive, setActive] = useState("false");
+  const ToggleClass = () => {
+    setActive(!isActive);
+  };
   return (
-    <nav className="nav">
-      <div className="icons">
-      <AccountCircleTwoToneIcon className="accountIcon"/>
-      <WbSunnyTwoToneIcon className="sunIcon"/>
-      <ChatTwoToneIcon className="chatIcon"/>
+    <nav className={isActive ? "dawn_nav" : "dusk_nav"}>
+      <div className={isActive ? "dawn_icons" : "dusk_icons"}>
+        <AccountCircleTwoToneIcon className="accountIcon" fontSize="large" />
+        <button onClick={ToggleClass} className="profileSwitchButton">
+          <WbSunnyTwoToneIcon className="sunIcon" fontSize="large" />
+        </button>
+        <ChatTwoToneIcon className="chatIcon" fontSize="large" />
       </div>
     </nav>
   );
