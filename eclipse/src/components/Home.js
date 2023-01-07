0bdf026b8a1f898e4c68bae877bc5973866d6868
Blake Sartin <blake.sartin8@gmail.com>
2023-01-07 15:57:52 -0800

    add buttons and styling for home component

--- eclipse/src/components/Home.js
+++ eclipse/src/components/Home.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./Home.scss"
+import FavoriteTwoToneIcon from '@mui/icons-material/FavoriteTwoTone';
+import HeartBrokenTwoToneIcon from '@mui/icons-material/HeartBrokenTwoTone';
 
 export default function Home () {
 return (
   <div className="home_container">
     <img className="profile_pic" src="../photos/profilepic.jpg"></img>
-    <h1></h1>
+    <h1>Katrina Teitz</h1>
     <div className="swipe_buttons">
-      <button></button>
-      <button></button>
-      <button></button>
+    <button className="heart_broken_button"><HeartBrokenTwoToneIcon fontSize="large"/></button>
+      <button className="heart_button"><FavoriteTwoToneIcon fontSize="large"/></button>
       </div>
     </div>
 )
