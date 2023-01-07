a116cf7e0c7e46df4a2830c3867a8de1a7f12344
Blake Sartin <blake.sartin8@gmail.com>
2023-01-07 15:52:04 -0800

    add default profile pic for home component

--- eclipse/src/components/Home.js
+++ eclipse/src/components/Home.js
@@ -4,7 +4,7 @@ import "./Home.scss"
 export default function Home () {
 return (
   <div className="home_container">
-    <img></img>
+    <img className="profile_pic" src="../photos/profilepic.jpg"></img>
     <h1></h1>
     <div className="swipe_buttons">
       <button></button>
